26ff5c472aca4597886ff82e2626adfab6985905
williammarinho93 <willmarinhosantos93@gmail.com>
2023-11-20 13:42:46 -0300

    firt commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declarando array dos nomes dos arquivos de imagem */
+
+/* Declarando o texto alternativo para cada arquivo de imagem */
+
+/* Percorrendo imagens */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+
